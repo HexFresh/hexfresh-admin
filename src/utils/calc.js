2b00d29c9d92e8b3723fe4256367fd95c25074c2
Hà Minh Cường <55825733+haminhcuong@users.noreply.github.com>
2022-02-21 21:38:02 +0700

    add utils

---
+++ src/utils/calc.js
@@ -0,0 +1,26 @@
+export const unrecordRoute = ['/signin', '/signup', '/change-password', '/courses', '/'];
+
+export const calculateRemainingTime = (expirationTime) => {
+  const currentTime = new Date().getTime();
+  const expiredTime = new Date(expirationTime).getTime();
+  const remainingTime = expiredTime - currentTime;
+  return remainingTime;
+};
+
+export const retrieveStoredToken = () => {
+  const storedToken = localStorage.getItem('token');
+  const storedExpirationDate = localStorage.getItem('expirationTime');
+
+  const remainingTime = calculateRemainingTime(storedExpirationDate);
+
+  if (remainingTime <= 3600) {
+    localStorage.removeItem('token');
+    localStorage.removeItem('expirationTime');
+    return null;
+  }
+
+  return {
+    token: storedToken,
+    duration: remainingTime,
+  };
+};
